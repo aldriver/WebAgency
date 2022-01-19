da54b041fab8fe3d9d763d434f0f6fbfd052b1ad
aldriver <adrianlouisdriver@gmail.com>
2022-01-19 12:40:11 -0800

    added an alert

---
+++ app.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+      alert(`
+        The purpose of the site was to practice styling responsive webpages using HTML and CSS. JavaScript was only used to display this message, and the buttons are not functional. The navbar at the top of the page is functional.
+        The contents of this page are based off of a YouTube tutorial by "Coding Addict", but I did the work with little assistance. The tutorial was mostly used to give me ideas for page layouts and transitions to practice implementing.
+      `) 
+})
